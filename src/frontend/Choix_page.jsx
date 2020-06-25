e14147b3ecfa42cf283b162031e569a20ea745f6
Yasser_Chouadra_ESI <hy_chouadra@esi.dz>
2020-06-25 13:53:25 +0100

    12

---
+++ src/frontend/Choix_page.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import HomeEP from './HomeEP';
+import CatDash from './CatDash';
+import ReactDom from 'react-dom';
+
+
+
+
+
+
+
+
+
+class Choix_page extends Component {
+  
+activer1 = event => {  
+  ReactDom.render(<HomeEP  />,document.getElementById('root'));   
+
+                         };
+
+
+  ///////////////////////////////////////////////////////////
+
+  activer2 = event => {
+  ReactDom.render(<CatDash />,document.getElementById('root'));   
+
+                         };
+    ///////////////////////////////////////////////////////////
+  render() { 
+        return  (
+               <div className="container text-center">   
+                 <h1>  Bienvenu  dans notre application   </h1>
+                    <h1> login avec secsess   </h1>
+                 <h1> Page de choix entre pendimie et catastrophe   </h1>
+      <button onClick={this.activer1}  className="btn btn-primary text-uppercase"> Dash Pendimie </button>
+      <button onClick={this.activer2}  className="btn btn-primary text-uppercase"> Dash catastrophe  </button>
+ 
+                 </div>
+                );
+    }
+}
+export default Choix_page;
---
+++ src/frontend/authentification.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import axios from 'axios';
+import Choix_page from './Choix_page';
+
+
+  
+
+
+
+
+class Authentification extends Component {
+            
+                  state = { 
+                    my_comptes :[],
+                    email_tapped :" ",
+                    password_tapped :" "
+                      }
+///////////////////     sss ////////////////////////////////////////////////////////////
+        componentDidMount(){
+            axios.get('http://localhost:5000/compte/')
+            .then( comptes => {
+                                    this.setState({ my_comptes : comptes.data })   
+                                })
+            .catch(function (error) { console.log(error);})
+        }     
+///////////////////////////////////////////////////////////////////////////////
+
+   login=(email,pswd) => {
+
+        const comptes = [...this.state.my_comptes];
+          comptes.map(compte =>{             
+          if(compte.mail === email && compte.password === pswd)
+          {
+            if (compte.Type ==="EP") {
+                
+                ReactDom.render(<Choix_page my_compte={compte} />,document.getElementById('root'));   
+            } 
+              
+            else{
+                console.log("pas de login ");
+               }                    
+          }
+                               })}  ;
+///////////////////////////////////////////////////////////////////////////////
+            handleChange1 = event => {
+               this.setState({ email_tapped: event.currentTarget.value });
+
+                                      };
+            handleChange2 = event => {
+                 this.setState({ password_tapped: event.currentTarget.value });
+                                        };
+            handleSubmit = event => {
+                           event.preventDefault();
+               this.login(this.state.email_tapped,this.state.password_tapped);
+                             };
+ render() { 
+        return  (     
+        <div >
+                                                  
+                                  <div > 
+                                  <div > 
+                                      <h1 > SIGN IN </h1>
+                                  </div>  
+                                    
+             <div className="container text-center"> 
+            <input   onChange={this.handleChange1} type="email" className="form-control" id="emailInput" placeholder="Adresse mail"></input>
+            <input  onChange={this.handleChange2} type="password" className="form-control" id="passwordInput" placeholder="Password"></input>                 
+            <button onClick={this.handleSubmit} type="submit" className="btn btn-primary text-uppercase"> Se connecter </button>
+                                      </div>
+                                   
+                                  </div>                                   
+                                              
+                                      
+                        </div>
+                           
+                        );
+        }
+}
+ 
+/////////////////////////////////////////////////////////////////////////////// 
+export default Authentification;
